ff10b03fb7fd3a2188db8cdf628814af429529b5
Riley Paul <rileypaul96@gmail.com>
2024-06-17 07:47:34 -0600

    tweak styles further

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -5,7 +5,7 @@ import { PocketKnife } from "lucide-react";
 export const Route = createRootRoute({
   component: () => (
     <>
-      <nav className="flex h-[3.5rem] items-center border-b">
+      <nav className="flex h-[3.5rem] items-center border-b bg-card">
         <div className="container2 flex w-full items-center gap-2">
           <Link to="/" className="flex items-center">
             <PocketKnife className="mr-3 h-6 w-6 text-primary" />
